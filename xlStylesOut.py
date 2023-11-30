923a06c639a82582f0142b55ef5d38b48b13b8f3
Dama Luke <601367179@qq.com>
2023-11-30 23:13:23 +0800

    把所有的参数都移到write_excel()中

--- xlStylesOut.py
+++ xlStylesOut.py
@@ -7,10 +7,7 @@ from tkinter import messagebox as msg
 
 # 输出带格式的excel
 class xlStyleFormat:
-    def __init__(self, path, dfs, headers=None):
-        self.path = path
-        self.dfs = dfs
-        self.headers = headers
+    def __init__(self):
         self.title_font = Font(
             name="MYingHei_18030_C-Medium", size=10, bold=True, color="00FFFFFF"
         )
@@ -28,7 +25,7 @@ class xlStyleFormat:
         self.align = Alignment(horizontal="center", vertical="center", wrapText=True)
         self.format_excel
         self.completedInfo
-
+        
     def format_col(self, cell):
         if cell.value:
             cell.font = self.title_font
@@ -46,10 +43,10 @@ class xlStyleFormat:
     def completedInfo(self):
         msg.showinfo("Completed", "已完成！")
 
-    def format_excel(self, info):
-        wb = openpyxl.load_workbook(self.path)
-        if self.headers != None:
-            heads = self.headers
+    def format_excel(self, path, headers, info):
+        wb = openpyxl.load_workbook(path)
+        if  headers != None:
+            heads = headers
         else:
             heads = 1
         for ws in wb:
@@ -61,13 +58,13 @@ class xlStyleFormat:
             ws.auto_filter.ref = f"A{heads}:{get_column_letter(col)}{heads}"
             for row in ws.iter_rows(min_row=2):
                 self.format_row(row)
-        wb.save(self.path)
+        wb.save(path)
         if info == True:
             self.completedInfo()
 
-    def write_excel(self, info=False):
-        with pd.ExcelWriter(self.path) as writer:
-            for sheet, df in self.dfs.items():
+    def write_excel(self, path, dfs, headers = None, info=False):
+        with pd.ExcelWriter(path) as writer:
+            for sheet, df in dfs.items():
                 df.to_excel(writer, sheet_name=sheet, index=False)
-        print("outfile path is in:" + self.path)
-        self.format_excel(info)
+        print("outfile path is in:" + path)
+        self.format_excel(path, headers, info)
