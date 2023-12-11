2db6db6825f05a7da658b8f59d4285ff073a3e06
Dama Luke <601367179@qq.com>
2023-12-11 11:30:59 +0800

    1.write_excel()不再封装在类里，而是单独作为一个方法使用。通过传入Namedstyle来对excel的title和body进行格式化处理。 2.增加rich.console的Console方法，在进行file写入操作时显示工作状态，并在最后打印出文件路径。 3.使用add_name_style直接渲染workbook为bodystyle格式，随后再单独渲染title格式。

--- xlStylesOut.py
+++ xlStylesOut.py
@@ -1,71 +1,76 @@
 import openpyxl
 from openpyxl.utils import get_column_letter
-from openpyxl.styles import PatternFill, Border, Side, Alignment, Font
+from openpyxl.styles import PatternFill, Border, Side, Alignment, Font, NamedStyle
 import pandas as pd
 from tkinter import messagebox as msg
+from rich.console import Console
 
 
-# 输出带格式的excel
-class xlStyleFormat:
-    def __init__(self):
-        self.title_font = Font(
-            name="MYingHei_18030_C-Medium", size=10, bold=True, color="00FFFFFF"
-        )
-        self.title_fill = PatternFill(
-            fill_type="solid", start_color="3D5AAE", end_color="3D5AAE"
-        )
-        self.body_font = Font(
-            name="MYingHei_18030_C-Medium", size=9, bold=False, color="000000"
-        )
-        self.body_fill = PatternFill(
-            fill_type="solid", start_color="00FFFFFF", end_color="00FFFFFF"
-        )
-        thin = Side(border_style="thin", color="000000")
-        self.border = Border(left=thin, right=thin, top=thin, bottom=thin)
-        self.align = Alignment(horizontal="center", vertical="center", wrapText=True)
-        self.format_excel
-        self.completedInfo
+# 默认格式
+title_font = Font(
+    name="MYingHei_18030_C-Medium", size=10, bold=True, color="00FFFFFF"
+)
+title_fill = PatternFill(
+    fill_type="solid", start_color="3D5AAE", end_color="3D5AAE"
+)
+body_font = Font(
+    name="MYingHei_18030_C-Medium", size=9, bold=False, color="000000"
+)
+body_fill = PatternFill(
+    fill_type="solid", start_color="00FFFFFF", end_color="00FFFFFF"
+)
+thin = Side(border_style="thin", color="000000")
+border = Border(left = thin, right = thin, top = thin, bottom = thin)
+align = Alignment(horizontal="center", vertical="center", wrapText=True)    
+
+title_namestyle = NamedStyle(name = "default title format",
+                               font = title_font,
+                               fill = title_fill,
+                               border = border,
+                               alignment = align)
+body_namestyle = NamedStyle(name = "default body format",
+                               font = body_font,
+                               fill = body_fill,
+                               border = border,
+                               alignment = align)
+    
+    
+def write_excel(path: str, 
+                dfs: dict[str, object],
+                headers = 1, 
+                titleStyle = title_namestyle,
+                bodyStyle = body_namestyle,                
+                isCompeleted = False) -> None:
+    """Write muti-dataframes into excel file and formatted that. 
+    
+    Args:
+        path: path to write the spreedsheets(str).
+        dfs: A dictionary of (sheet names:keys, DataFrames:values) in pairs.
+        headers: The number of rows of title.
+        titleStyle: The style of title. It's a openpyxl.styles.NamedStyle class.
+        bodyStyle: The style of body. It's a openpyxl.styles.NamedStyle class.
+        isCompeleted: Whether to showinfo of is completed."""
         
-    def format_col(self, cell):
-        if cell.value:
-            cell.font = self.title_font
-            cell.fill = self.title_fill
-            cell.border = self.border
-            cell.alignment = self.align
-
-    def format_row(self, row):
-        for cell in row:
-            cell.font = self.body_font
-            cell.fill = self.body_fill
-            cell.border = self.border
-            cell.alignment = self.align
-
-    def completedInfo(self):
-        msg.showinfo("Completed", "已完成！")
-
-    def format_excel(self, path, headers, info):
-        wb = openpyxl.load_workbook(path)
-        if  headers != None:
-            heads = headers
-        else:
-            heads = 1
-        for ws in wb:
-            for col in range(ws.min_column, ws.max_column + 1):
-                ws.column_dimensions[get_column_letter(col)].auto_size = True
-                for i in range(1, heads + 1):
-                    cell = ws.cell(i, col)
-                    self.format_col(cell)
-            ws.auto_filter.ref = f"A{heads}:{get_column_letter(col)}{heads}"
-            for row in ws.iter_rows(min_row=2):
-                self.format_row(row)
-        wb.save(path)
-        if info == True:
-            self.completedInfo()
-
-    def write_excel(self, path, dfs, headers = None, info=False):
+    def completedInfo():
+        msg.showinfo("Completed", "已完成！")    
+    
+    console = Console()
+    with console.status("Writing Excel to {}".format(path)):  
         with pd.ExcelWriter(path) as writer:
             for sheet, df in dfs.items():
                 df.to_excel(writer, sheet_name=sheet, index=False)
-                df = df.drop_duplicates() # drop duplicate before output
-        print("outfile path is in:" + path)
-        self.format_excel(path, headers, info)
+                df = df.drop_duplicates() 
+        wb = openpyxl.load_workbook(path)
+        wb.add_named_style(bodyStyle)
+        for ws in wb:
+            for col in range(ws.min_column, ws.max_column + 1):
+                ws.column_dimensions[get_column_letter(col)].auto_size = True
+                for i in range(1, headers + 1):
+                    cell = ws.cell(i, col)
+                    cell.style = titleStyle
+            ws.auto_filter.ref = f"A{headers}:{get_column_letter(col)}{headers}"
+        wb.save(path)
+    
+    print("文件路径为 " + path)
+    if isCompeleted == True:
+        completedInfo()       
