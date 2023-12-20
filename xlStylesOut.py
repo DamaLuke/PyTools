139412b75214e2c91c49d822ca10cd9075857ecf
Dama Luke <601367179@qq.com>
2023-12-20 12:14:07 +0800

    xlStylesOut.py : 字体改为微软雅黑，由于add_namedstyle无法改变整个workbook格式，所以改回为行循环

--- xlStylesOut.py
+++ xlStylesOut.py
@@ -8,20 +8,20 @@ from rich.console import Console
 
 # 默认格式
 title_font = Font(
-    name="MYingHei_18030_C-Medium", size=10, bold=True, color="00FFFFFF"
+    name=u"微软雅黑", size=10, bold=True, color="00FFFFFF"
 )
 title_fill = PatternFill(
     fill_type="solid", start_color="3D5AAE", end_color="3D5AAE"
 )
 body_font = Font(
-    name="MYingHei_18030_C-Medium", size=9, bold=False, color="000000"
+    name=u"微软雅黑", size=9, bold=False, color="000000"
 )
 body_fill = PatternFill(
     fill_type="solid", start_color="00FFFFFF", end_color="00FFFFFF"
 )
 thin = Side(border_style="thin", color="000000")
 border = Border(left = thin, right = thin, top = thin, bottom = thin)
-align = Alignment(horizontal="center", vertical="center", wrapText=True)    
+align = Alignment(horizontal="center", vertical="center")    
 
 title_namestyle = NamedStyle(name = "default title format",
                                font = title_font,
@@ -33,7 +33,6 @@ body_namestyle = NamedStyle(name = "default body format",
                                fill = body_fill,
                                border = border,
                                alignment = align)
-    
     
 def write_excel(path: str, 
                 dfs: dict[str, object],
@@ -58,16 +57,20 @@ def write_excel(path: str,
     with console.status("Writing Excel to {}".format(path)):  
         with pd.ExcelWriter(path) as writer:
             for sheet, df in dfs.items():
+                df = df.drop_duplicates()
                 df.to_excel(writer, sheet_name=sheet, index=False)
-                df = df.drop_duplicates() 
         wb = openpyxl.load_workbook(path)
         wb.add_named_style(bodyStyle)
         for ws in wb:
             for col in range(ws.min_column, ws.max_column + 1):
-                ws.column_dimensions[get_column_letter(col)].auto_size = True
+                ws.column_dimensions[get_column_letter(col)].bestFit = True
                 for i in range(1, headers + 1):
-                    cell = ws.cell(i, col)
-                    cell.style = titleStyle
+                    title_cell = ws.cell(i, col)
+                    title_cell.style = titleStyle
+                    for row in range(ws.min_row + 1, ws.max_row + 1):
+                        body_cell = ws.cell(row, col)
+                        body_cell.style = bodyStyle
+
             ws.auto_filter.ref = f"A{headers}:{get_column_letter(col)}{headers}"
         wb.save(path)
     
